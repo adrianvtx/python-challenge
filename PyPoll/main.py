a42a54ec815207c4b96ec2196c90185562d61c9a
adrianvtx <adrian.voj.2019@gmail.com>
2019-06-28 23:53:11 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,43 +3,45 @@ import os
 
 # Module for reading CSV files
 import csv
-
-csvpath = os.path.join("Resources","election_data.csv")
-cand = []
+# election_data.csv
+csvpath = os.path.join("PyPoll","Resources","election_data1.csv")
+cand = {}
 cand_nm = []
 vote = []
 ttlv = 0
-iter=0
+count = 0
+elDat={}
 
 with open(csvpath, newline='') as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
-    elData = csv.reader(csvfile, delimiter=',')
+    # elData = csv.reader(csvfile, delimiter=',')
     # Read the header row first (skip this step if there is now header)
-    csv_header = next(elData)
-    print('A')
-    
-    for row in elData:
-        cand.append(row[2])
-        for uniq in cand:
-            if uniq not in cand_nm:
-                cand_nm.append(uniq)
-            iter=iter+1
-            print(iter)
-    
-    for x in cand_nm:
-        nmvt = cand.count(x)
-        vote.append(nmvt)
-        print("C")
-    
+    csv_header = next(csvfile)
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        count += 1
+        candidate = row[2]
+        if candidate not in cand:
+            cand[candidate] = 1
+        else:
+            cand[candidate] += 1
+
+print(cand[0]
+
+for x in cand
+    nmvt = len(cand)
+    vote.append(nmvt)
+    print("C")
+        
     #print(vote)
-    ttlv = len(cand)
+ttlv = len(cand)
     #print(ttlv)
-    pctV = [(a*100) / ttlv for a in (vote)]
+pctV = [(a*100) / ttlv for a in (vote)]
     #print(pctV)
-    wins = cand_nm[pctV.index(max(pctV))]
+wins = cand[pctV.index(max(pctV))]
     #print(wins)
-    print("Election Results \n------------------------------")
-    print(f"Total Votes: {len(cand)} \n------------------------------")
-    for i in range(len(cand_nm)):
-        print(f"{cand_nm[i]}: {round(pctV[i],3)}% ({vote[i]})")
-    print(f"------------------------------\n Winner: {wins}\n------------------------------")
+print("Election Results \n------------------------------")
+print(f"Total Votes: {len(cand)} \n------------------------------")
+for i in range(len(cand_nm)):
+    print(f"{cand_nm[i]}: {round(pctV[i],3)}% ({vote[i]})")
+print(f"------------------------------\n Winner: {wins}\n------------------------------")

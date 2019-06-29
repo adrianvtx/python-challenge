f53bdfb98cc4a1604cc1ee513d4e0b1e8accf6e1
adrianvtx <adrian.voj.2019@gmail.com>
2019-06-29 18:45:34 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,17 @@
 # First we'll import the os module
 import os
-
 # Module for reading CSV files
 import csv
 # election_data.csv
 csvpath = os.path.join("PyPoll","Resources","election_data1.csv")
 cand = {}
 cand_nm = []
-vote = []
-ttlv = 0
+vote = 0
+vote1 = 0
+nmvt = 0
 count = 0
-elDat={}
+elDat = {}
+winner=str
 
 with open(csvpath, newline='') as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
@@ -18,30 +19,44 @@ with open(csvpath, newline='') as csvfile:
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvfile)
     csvreader = csv.reader(csvfile)
+    
     for row in csvreader:
         count += 1
         candidate = row[2]
+        # Vote1=(cand.get(candidate))
         if candidate not in cand:
             cand[candidate] = 1
+            # cand[vote] = 1
         else:
             cand[candidate] += 1
 
-print(cand[0]
-
-for x in cand
-    nmvt = len(cand)
-    vote.append(nmvt)
-    print("C")
+print(cand.get(candidate))
         
-    #print(vote)
-ttlv = len(cand)
-    #print(ttlv)
-pctV = [(a*100) / ttlv for a in (vote)]
-    #print(pctV)
-wins = cand[pctV.index(max(pctV))]
-    #print(wins)
+vMax = max(cand.keys(), key=(lambda k: cand[k]))
+vMin = min(cand.keys(), key=(lambda k: cand[k]))
+print(vMax)
+print(vMin)
+print(cand)
+ttlv = count
+print(ttlv)
+
+# Also Unnecessary
+# wins = list(cand)
+# print(wins)
+# Winner = wins[wins.index(max(cand))]
+# print(winner)
+
+
+    #text output
 print("Election Results \n------------------------------")
-print(f"Total Votes: {len(cand)} \n------------------------------")
-for i in range(len(cand_nm)):
-    print(f"{cand_nm[i]}: {round(pctV[i],3)}% ({vote[i]})")
-print(f"------------------------------\n Winner: {wins}\n------------------------------")
+print(f"Total Votes: {count} \n------------------------------")
+print(f"Winner: {vMax} \n------------------------------")
+for key, value in cand.items():
+    print(f"{key}:  {round(value/count*100,3)}%   ({value})")
+
+#         wins.append(key) Alternate append to list not needed
+#         wins.append(value)
+# for i in range(len(cand)):
+    # pctv = ((value) / (vMax))
+    # print(f"{key}: {round(pctV[i],3)}% ({value})")
+print(f"------------------------------\n Winner: {vMax}\n------------------------------")
